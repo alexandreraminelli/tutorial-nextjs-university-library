b2eea2e113f8e01ee76b9f7cf1d45903d91ea7ff
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-03-21 21:46:54 -0300

    feat(layout): adição de fundo e estrutura ao layout principal

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -4,9 +4,13 @@
  */
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="root-container">
-      {/* Página */}
-      {children}
+    <main className="root-container bg-pattern">
+      <div className="mx-auto max-w-7xl">
+        {/* Header */}
+        Header
+        {/* Página */}
+        <div className="mt-20 pb-20">{children}</div>
+      </div>
     </main>
   )
 }
