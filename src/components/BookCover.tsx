23790094b661157d40c10b0b9afc8b6f86e1061c
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-03-23 19:22:43 -0300

    feat(book-overview): criação dos componentes BookOverview e BookCover

---
+++ src/components/BookCover.tsx
@@ -0,0 +1,45 @@
+import { cn } from "@/lib/utils"
+import Image from "next/image"
+
+/** Variações de estilos da capa. */
+type BookCoverVariant = "extraSmall" | "small" | "medium" | "regular" | "wide"
+/** Variação de estilos da capa. */
+const variantStyles: Record<BookCoverVariant, string> = {
+  extraSmall: "book-cover_extra_small",
+  small: "book-cover_small",
+  medium: "book-cover_medium",
+  regular: "book-cover_regular",
+  wide: "book-cover_wide",
+}
+
+/** Tipos dos props de `BookCover`. */
+interface Props {
+  /** Classes adicionais. (opcional) */
+  className?: string
+  /** Variante da capa. (opcional) */
+  variant?: BookCoverVariant
+  /** Cor da capa. */
+  coverColor: string
+  /** URL da capa. */
+  coverImage: string
+}
+
+/** Imagem da capa de um livro. */
+export default function BookCover(
+  { className, variant = "regular", coverColor = "#012B48", coverImage: coverURL = "https://placehold.co/400x600.png" }: Props // props
+) {
+  return (
+    <div
+      className={cn(
+        "relative transition-all duration-300", // estilo base
+        variantStyles[variant], // estilo da variante
+        className // classes adicionais
+      )}
+    >
+      <div className="absolute z-10" style={{ left: "12%", width: "87.5%", height: "88%" }}>
+        {/* Imagem da capa */}
+        <Image src={coverURL} alt="Book cover" fill className="rounded-sm object-fill" />
+      </div>
+    </div>
+  )
+}
--- src/components/BookOverview.tsx
+++ src/components/BookOverview.tsx
@@ -1,4 +1,66 @@
+import { Button } from "@/components/ui/button"
+import BookCover from "@/components/BookCover"
+import Image from "next/image"
+
 /** Informações gerais sobre um livro. */
-export default function BookOverview() {
-  return <section className="book-overview">Book Overview</section>
+export default function BookOverview(
+  { title, author, genre, rating, total_copies, available_copies, description, color, cover }: Book // props
+) {
+  return (
+    <section className="book-overview">
+      <div className="flex flex-1 flex-col gap-5">
+        {/* Título */}
+        <h1>{title}</h1>
+
+        <div className="book-info">
+          {/* Autor */}
+          <p>
+            By <span className="font-semibold text-light-200">{author}</span>
+          </p>
+          {/* Categoria/Gênero */}
+          <p>
+            Category: <span className="font-semibold text-light-200">{genre}</span>
+          </p>
+          {/* Avaliação */}
+          <div className="flex flex-row gap-1">
+            {/* Ícone de estrela */}
+            <Image src="/icons/star.svg" alt="star" width={22} height={22} />
+            <p>{rating}</p>
+          </div>
+        </div>
+
+        {/* Informação de copias disponíveis */}
+        <div className="book-copies">
+          <p>
+            Total Books: <span>{total_copies}</span>
+          </p>
+          <p>
+            Available Books: <span>{available_copies}</span>
+          </p>
+        </div>
+
+        {/* Descrição */}
+        <p className="book-description">{description}</p>
+
+        {/* CTA Button */}
+        <Button className="book-overwiew_btn">
+          {/* Ícone de livro */}
+          <Image src="/icons/book.svg" alt="book" width={20} height={20}></Image>
+          <p className="font-bebas-neue text-xl text-dark-100c">Borrow Book</p>
+        </Button>
+      </div>
+
+      {/* Capa do livro */}
+      <div className="relative flex flex-1 justify-center">
+        {/* Imagem normal */}
+        <div className="relative">
+          <BookCover variant="wide" className="z-10" coverColor={color} coverImage={cover} />
+        </div>
+        {/* "Sombra" da imagem */}
+        <div className="absolute left-16 top-10 rotate-12 opacity-40 max-sm:hidden">
+          <BookCover variant="wide" coverColor={color} coverImage={cover} />
+        </div>
+      </div>
+    </section>
+  )
 }
