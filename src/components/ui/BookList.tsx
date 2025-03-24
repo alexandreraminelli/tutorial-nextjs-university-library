b46f814c866f7e4699c9b60a889fa34863f5e36b
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-03-23 21:11:21 -0300

    feat(book-list): criação do componente de lista de livros

--- src/components/ui/BookList.tsx
+++ src/components/ui/BookList.tsx
@@ -1,4 +1,30 @@
+import BookCard from "@/components/BookCard"
+
+/** Props de `BookList`. */
+interface Props {
+  /** Título da lista. */
+  title: string
+  /** Livros da lista. */
+  books: Book[]
+  /** Classe CSS do container. (opcional) */
+  containerClassName?: string
+}
+
 /** Lista de livros. */
-export default function BookList() {
-  return <div>Book List</div>
+export default function BookList(
+  { title, books, containerClassName }: Props // props
+) {
+  return (
+    <section className={containerClassName}>
+      {/* Título da lista */}
+      <h2 className="font-bebas-neue text-4xl text-light-100">{title}</h2>
+
+      {/* Lista de livros */}
+      <ul className="book-list">
+        {books.map((book) => (
+          <BookCard key={book.title} {...book} />
+        ))}
+      </ul>
+    </section>
+  )
 }
