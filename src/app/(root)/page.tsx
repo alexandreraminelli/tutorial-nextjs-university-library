c7b60e2c118258be085c38d77df81ec83522c897
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-03-23 21:12:16 -0300

    feat(homepage): adição do BookOverview e BookList na página inicial

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,15 +1,16 @@
 import BookOverview from "@/components/BookOverview"
 import BookList from "@/components/ui/BookList"
+import { sampleBooks } from "@/constants"
 
 /** Homepage da plataforma do usuário. */
 export default function HomePage() {
   return (
     <>
       {/* Hero Section com informações de um livro */}
-      <BookOverview />
+      <BookOverview {...sampleBooks[0]} />
 
       {/* Lista de livros */}
-      <BookList />
+      <BookList title="Latest Books" books={sampleBooks} containerClassName="mt-28" />
     </>
   )
 }
