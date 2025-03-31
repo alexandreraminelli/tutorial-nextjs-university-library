e57ddd736067aa9f5e266f8e0b656872a5851741
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-03-31 19:42:55 -0300

    feat(auth): aprimorar layout das páginas de autenticação com logotipo e ilustração

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,28 @@
+import Image from "next/image"
+
 /** Layout das rotas de autenticação. */
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>
+  return (
+    <main className="auth-container">
+      <section className="auth-form">
+        <div className="auth-box gradient-vertical">
+          {/* Logotipo do site */}
+          <div className="flex flex-row gap-3">
+            <Image src="/icons/logo.svg" alt="Logo" width={37} height={37} className="mb-4" />
+            <h1 className="text-2xl font-semibold text-white">BookWise</h1>
+          </div>
+
+          <div>
+            {/* Página */}
+            {children}
+          </div>
+        </div>
+      </section>
+
+      {/* Imagem decorativa de livros */}
+      <section className="auth-illustration">
+        <Image src="/images/auth-illustration.png" alt="auth illustration" height={1000} width={1000} className="size-full object-cover" />
+      </section>
+    </main>
+  )
 }
