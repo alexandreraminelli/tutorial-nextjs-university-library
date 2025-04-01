a2652306bddbd802c14f107d62316f18a31fc156
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-03-31 21:17:28 -0300

    feat(auth): adição do formulário nas páginas de autenticação

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,4 +1,16 @@
+"use client"
+
+import AuthForm from "@/components/AuthForm"
+import { signInSchema } from "@/lib/validations"
+
 /** Página de login. */
 export default function SignInPage() {
-  return <div></div>
+  return (
+    <AuthForm
+      type="SIGN_IN" // tipo de formulário
+      schema={signInSchema}
+      defaultValues={{ email: "", password: "" }} // valores padrão: strings vazias
+      onSubmit={() => {}} // função para lidar com o envio do formulário
+    />
+  )
 }
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,4 +1,16 @@
+"use client"
+
+import AuthForm from "@/components/AuthForm"
+import { signUpSchema } from "@/lib/validations"
+
 /** Página de criar conta. */
 export default function SignUpPage() {
-  return <div></div>
+  return (
+    <AuthForm
+      type="SIGN_UP" // tipo de formulário
+      schema={signUpSchema}
+      defaultValues={{ email: "", password: "", fullName: "", universityId: 0, universityCard: "" }} // valores padrão: strings vazias e 0
+      onSubmit={() => {}} // função para lidar com o envio do formulário
+    />
+  )
 }
