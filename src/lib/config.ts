504c995d3c1ed09ef64289e6ac61918ba9362d3e
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-06-13 12:29:32 -0300

    feat(imagekit/env): carregamento das variáveis de ambiente no arquivo de configuração

---
+++ src/lib/config.ts
@@ -0,0 +1,19 @@
+/**
+ * Arquivo de configuração do projeto.
+ * Contém as configurações de ambiente e outras variáveis globais.
+ */
+const config = {
+  env: {
+    /** Variáveis de ambiente para conexão com o ImageKit */
+    imagekit: {
+      /** URL do endpoint do ImageKit. */
+      urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT,
+      /** Chave pública da API do ImageKit. */
+      publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
+      /** Chave privada da API do ImageKit. */
+      privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+    },
+  },
+}
+
+export default config
