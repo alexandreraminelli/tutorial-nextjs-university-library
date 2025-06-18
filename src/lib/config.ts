cf52903d8f9d2c30f441e5b109b4028b9a3a049a
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-06-18 13:37:56 -0300

    feat(env/database): variáveis de ambiente para conexão com o banco de dados

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,9 +3,15 @@
  * Contém as configurações de ambiente e outras variáveis globais.
  */
 const config = {
+  // `!`: Indica ao TS que a variável é obrigatório e não pode ser `undefined`.
   env: {
     /** Endpoint da API do Next.js criada nesse repositório. */
     apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
+    /** Variáveis de ambiente para o banco de dados. */
+    database: {
+      /** URL de conexão com o banco de dados */
+      databaseUrl: process.env.DATABASE_URL!,
+    },
     /** Variáveis de ambiente para conexão com o ImageKit. */
     imagekit: {
       /** URL do endpoint do ImageKit. */
