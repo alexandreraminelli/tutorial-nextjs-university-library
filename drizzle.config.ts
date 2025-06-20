975bccd5068a92bde177e7c80ce63f0015b0c11a
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-06-20 17:50:48 -0300

    feat(drizzle/config): configuração do Drizzle ORM

---
+++ drizzle.config.ts
@@ -0,0 +1,23 @@
+/*
+ * Configuração do Drizzle ORM.
+ * Define as opções de migração e conexão com o banco de dados.
+ */
+
+import { config } from "dotenv"
+import { defineConfig } from "drizzle-kit"
+
+config({ path: ".env.local" }) // Carregar variáveis de ambiente
+
+// Configurações do Drizzle ORM
+export default defineConfig({
+  // Caminho para o esquema do banco de dados.
+  schema: "./src/database/schema.ts",
+  // Caminho para as migrações (arquivos SQL) geradas.
+  out: "./migrations",
+  // Dialeto do banco de dados utilizado (MySQL, PostgreSQL, etc).
+  dialect: "postgresql",
+  // Credenciais do banco de dados.
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+})
