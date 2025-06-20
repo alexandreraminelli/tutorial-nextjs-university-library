0b5f607b0c435b4dbf34d6cab33ccf6ad5345cf5
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-06-20 20:40:28 -0300

    test(database): obter dados da tabela `users`

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,16 @@
 import BookOverview from "@/components/BookOverview"
 import BookList from "@/components/ui/BookList"
 import { sampleBooks } from "@/constants"
+import { db } from "@/database/drizzle"
+import { users } from "@/database/schema"
 
 /** Homepage da plataforma do usuário. */
-export default function HomePage() {
+export default async function HomePage() {
+  const result = await db.select().from(users)
+  console.log(
+    JSON.stringify(result, null, 2) // Exibe os dados do usuário no console
+  )
+
   return (
     <>
       {/* Hero Section com informações de um livro */}
