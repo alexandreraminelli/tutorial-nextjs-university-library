a62a58c33f3ff31ab0949c5c5378d0d8994bc315
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-06-21 17:30:02 -0300

    feat(auth/navigation): redirecionar usuários não logados a tela de autenticação

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,10 +1,16 @@
+import { auth } from "@/auth"
 import Header from "@/components/Header"
+import { redirect } from "next/navigation"
 
 /**
  * Layout com componentes principais da aplicação, como navbar e footer.
  * Não é aplicado a todas as páginas (por exemplo, a página de login).
  */
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  // Redirecionar usuários não autenticados para a tela de autenticação
+  const session = await auth()
+  if (!session) redirect("/")
+
   return (
     <main className="root-container bg-pattern">
       <div className="mx-auto max-w-7xl">
