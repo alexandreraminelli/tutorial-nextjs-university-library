ba7df21e3611e8a6200c4cab35f3d1c741a00587
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-06-23 19:45:17 -0300

    feat(session): registrar a data da última atividade do usuário

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,6 +1,10 @@
 import { auth } from "@/auth"
 import Header from "@/components/Header"
+import { db } from "@/database/drizzle"
+import { users } from "@/database/schema"
+import { eq } from "drizzle-orm"
 import { redirect } from "next/navigation"
+import { after } from "next/server"
 
 /**
  * Layout com componentes principais da aplicação, como navbar e footer.
@@ -10,6 +14,22 @@ export default async function Layout({ children }: { children: React.ReactNode }
   // Redirecionar usuários não autenticados para a tela de autenticação
   const session = await auth()
   if (!session) redirect("/sign-in")
+
+  // Executar código após a renderização do layout (evitar bloqueio de renderização)
+  after(async () => {
+    // Encerra se o usuário não estiver autenticado
+    if (!session?.user?.id) return
+
+    // Se o usuário já tiver uma atividade registrada hoje, não precisa fazer nada
+    const user = await db.select().from(users).where(eq(users.id, session.user.id)).limit(1)
+    if (user[0].lastActivityDate === new Date().toISOString().slice(0, 10)) return
+    else
+      // Atualizar a data da última atividade do usuário
+      await db
+        .update(users)
+        .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
+        .where(eq(users.id, session?.user?.id))
+  })
 
   return (
     <main className="root-container bg-pattern">
