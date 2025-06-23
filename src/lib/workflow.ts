04eeac924bdc97294fa69250ebdeead423d03cbe
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-06-23 19:29:25 -0300

    feat(email): criação de workflow para enviar um e-mail ao usuário

--- src/lib/workflow.ts
+++ src/lib/workflow.ts
@@ -1,5 +1,5 @@
 import config from "@/lib/config"
-import { Client as QStash, resend } from "@upstash/qstash"
+import { Client as QStashClient, resend } from "@upstash/qstash"
 import { Client as WorkflowClient } from "@upstash/workflow"
 
 /**
@@ -10,18 +10,33 @@ export const workflowClient = new WorkflowClient({
   token: config.env.upstash.qstashToken, // Token de autenticação do Qstash
 })
 
-const client = new Client({ token: "<QSTASH_TOKEN>" })
-
-// Envio de e-mail com Resend
-await client.publishJSON({
-  api: {
-    name: "email",
-    provider: resend({ token: "<RESEND_TOKEN>" }),
-  },
-  body: {
-    from: "Acme <onboarding@resend.dev>",
-    to: ["delivered@resend.dev"],
-    subject: "Hello World",
-    html: "<p>It works!</p>",
-  },
-})
+/**
+ * Cliente QStash para enviar mensagens.
+ * Utilizado para enviar e-mails através do Resend.
+ */
+const qstashClient = new QStashClient({ token: config.env.upstash.qstashToken })
+/**
+ * Função para enviar um e-mail utilizando o QStash e Resend.
+ *
+ * @param {Object} params - Parâmetros para enviar o e-mail.
+ * @param params.email - Endereço de e-mail do destinatário.
+ * @param params.subject - Assunto do e-mail.
+ * @param params.message - Mensagem do e-mail.
+ *
+ * @return {Promise<void>} Promise que resolve quando o e-mail for enviado.
+ * @throws {Error} Se ocorrer um erro ao enviar o e-mail.
+ */
+export async function sendEmail({ email, subject, message }: { email: string; subject: string; message: string }) {
+  return await qstashClient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.resendToken }),
+    },
+    body: {
+      from: "Alexandre Raminelli <library.alexandreraminelli.me>",
+      to: [email], // Destinatário
+      subject: subject, // Assunto
+      html: message, // Mensagem em HTML
+    },
+  })
+}
