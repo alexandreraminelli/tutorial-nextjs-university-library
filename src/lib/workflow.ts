17f74d1bd93bebdc35cc9b411432eb19cd7b0c81
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-06-23 19:15:42 -0300

    feat(email/env): variáveis de ambiente do Resend

--- src/lib/workflow.ts
+++ src/lib/workflow.ts
@@ -1,5 +1,6 @@
-import { Client as WorkflowClient } from "@upstash/workflow"
 import config from "@/lib/config"
+import { Client as QStash, resend } from "@upstash/qstash"
+import { Client as WorkflowClient } from "@upstash/workflow"
 
 /**
  * Workflow Client para interagir com o Upstash Workflow.
@@ -7,4 +8,20 @@ import config from "@/lib/config"
 export const workflowClient = new WorkflowClient({
   baseUrl: config.env.upstash.qstashUrl, // URL do Qstash
   token: config.env.upstash.qstashToken, // Token de autenticação do Qstash
+})
+
+const client = new Client({ token: "<QSTASH_TOKEN>" })
+
+// Envio de e-mail com Resend
+await client.publishJSON({
+  api: {
+    name: "email",
+    provider: resend({ token: "<RESEND_TOKEN>" }),
+  },
+  body: {
+    from: "Acme <onboarding@resend.dev>",
+    to: ["delivered@resend.dev"],
+    subject: "Hello World",
+    html: "<p>It works!</p>",
+  },
 })
