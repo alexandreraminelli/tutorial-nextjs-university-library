cbc12d6c371d4ad95ac6e0a36268abfa782596ed
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-07-02 18:20:36 -0300

    feat(database/schema): criação da tabela de registros de empréstimos de livros

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -75,3 +75,19 @@ export const books = pgTable("books", {
   /** Data de inserção do livro no sistema. */
   createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
 })
+
+/** Tabela de registros de empréstimos de livros. */
+export const borrowRecords = pgTable("borrow_records", {
+  id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
+  userId: uuid("user_id")
+    .references(() => users.id)
+    .notNull(),
+  bookId: uuid("book_id")
+    .references(() => books.id)
+    .notNull(),
+  borrowDate: timestamp("borrow_date", { withTimezone: true }).defaultNow().notNull(),
+  dueDate: date("due_date").notNull(),
+  returnDate: date("return_date"),
+  status: BORROW_STATUS_ENUM("status").default("BORROWED").notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
+})
